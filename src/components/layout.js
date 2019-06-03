998f506d466e47c42f9d96d5b23798c99a77a9ee
Fisher <jackfisher94@live.co.uk>
2019-06-03 22:23:23 +0100

    complete homepage designs

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,9 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import '../styles/style.css';
+
+// import "./layout.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,19 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <div className="homepage-container">
-        <div className="homepage-img-container">
-          <h1>Personal blog.</h1>
-        </div>
-        <div className="homepage-top-square">
-
-        </div>
-        <div className="homepage-bottom-square">
-
-        </div>
+      <div className="homepage-img-container">
+        <h1 className="homepage-title">Personal blog.</h1>
+        <Link to="#" className="read-latest-btn">Read Latest</Link>
+      </div>
+      <div className="homepage-top-square"><p className="intro-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p></div>
+      <div className="homepage-bottom-square">
+        <h3>Sign Up</h3>
+        <p>Join the mailing list to get the latest blogs.</p>
+        <form>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+        </form>
+      </div>
     </div>
   </Layout>
 )
