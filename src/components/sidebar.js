5ac9f4017f3c5533db2f3b18324bdb21151d0ed0
Fisher <jackfisher94@live.co.uk>
2019-06-07 18:16:58 +0100

    replace dummy maillist form with social links

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -9,13 +9,18 @@ const SideBar = () => {
         </p>
       </div>
       <div className="homepage-bottom-square">
-        <h3>Sign Up</h3>
-        <p className="form-text">Join the mailing list to get the latest blogs.</p>
-        <form action="https://gmail.us20.list-manage.com/subscribe/post?u=5143fe4e43d65fa5865d05076&amp;id=d82a3b0cac" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+        <h3>Social Links</h3>
+        <div style={{textAlign: "left"}}>
+        <p style={{marginBottom: "1rem"}}><a href="https://twitter.com/jckfshr" target="_blank" rel="noopener noreferrer">Find me on Twitter</a></p>
+        <p><a href="mailto:jackfisher5882@gmail.com">Send me an email</a></p>
+        </div>
+        {/* <h3>Sign Up</h3>
+        <p className="form-text">Join the mailing list to get the latest blogs.</p> */}
+        {/* <form action="https://gmail.us20.list-manage.com/subscribe/post?u=5143fe4e43d65fa5865d05076&amp;id=d82a3b0cac" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <button style={{cursor: "pointer"}} id="join-mailing" type="submit">Join</button>
-        </form>
+        </form> */}
         <div>
           
         </div>
