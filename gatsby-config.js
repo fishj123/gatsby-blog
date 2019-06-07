726502437686e2f2e99a38e0c14f60dce50f9178
Fisher <jackfisher94@live.co.uk>
2019-06-07 16:35:10 +0100

    build production

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,20 +6,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "JackFisherBlog",
-        short_name: "Blog",
-        start_url: "/",
-        background_color: "#0077bc",
-        theme_color: "#ffe45a",
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "src/images/icon.png", // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
