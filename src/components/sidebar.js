34fb0477d3b948d5426d5934c1885e14b4589ab5
Fisher <jackfisher94@live.co.uk>
2019-06-07 18:20:08 +0100

    add white border to social links

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -5,14 +5,33 @@ const SideBar = () => {
     <React.Fragment>
       <div className="homepage-top-square">
         <p className="intro-text">
-          Welcome to my website. There's not much to it, just a simple blog built with Gatsby. Have a look around and read some of my articles, if you like them then follow me on Twitter to keep up to date with the latest posts.
+          Welcome to my website. There's not much to it, just a simple blog
+          built with Gatsby. Have a look around and read some of my articles, if
+          you like them then follow me on Twitter to keep up to date with the
+          latest posts.
         </p>
       </div>
       <div className="homepage-bottom-square">
-        <h3>Social Links</h3>
-        <div style={{textAlign: "left"}}>
-        <p style={{marginBottom: "1rem"}}><a href="https://twitter.com/jckfshr" target="_blank" rel="noopener noreferrer">Find me on Twitter</a></p>
-        <p><a href="mailto:jackfisher5882@gmail.com">Send me an email</a></p>
+        <div
+          style={{
+            textAlign: "left",
+            border: "2px solid white",
+            padding: "1rem",
+          }}
+        >
+          <h3>Social Links</h3>
+          <p style={{ marginBottom: "1rem" }}>
+            <a
+              href="https://twitter.com/jckfshr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Find me on Twitter
+            </a>
+          </p>
+          <p>
+            <a href="mailto:jackfisher5882@gmail.com">Send me an email</a>
+          </p>
         </div>
         {/* <h3>Sign Up</h3>
         <p className="form-text">Join the mailing list to get the latest blogs.</p> */}
@@ -21,10 +40,7 @@ const SideBar = () => {
           <input type="email" placeholder="Email" />
           <button style={{cursor: "pointer"}} id="join-mailing" type="submit">Join</button>
         </form> */}
-        <div>
-          
-        </div>
-                      
+        <div />
       </div>
     </React.Fragment>
   )
