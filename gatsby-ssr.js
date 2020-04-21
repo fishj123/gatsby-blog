07216e2fddc0034b693e5e3cc10f891f4f145185
Jack <jackfisher94@live.co.uk>
2020-04-21 18:13:39 +0100

    fix context error not server rendering

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,12 @@
  */
 
 // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+import React from "react"
+import "./src/styles/style.css"
+import ThemeContextProvider from "./src/contexts/themeContext"
+require("prismjs/themes/prism-solarizedlight.css")
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeContextProvider>{element}</ThemeContextProvider>
+)
